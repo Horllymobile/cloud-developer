ae3861d3f0751f0785ed48a01be7e84c204c3d57
James Abraham <horlamidex1@gmail.com>
2022-07-15 13:47:28 +0100

    Used AJV to validate create and update todo data

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -3,37 +3,59 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
+import { getUserId } from '../utils'
 import { createTodo } from '../../helpers/todos'
 import { createLogger } from '../../utils/logger'
+import Ajv from 'ajv'
 
+const ajv = new Ajv()
 const logger = createLogger('createTodo')
-
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     logger.info('Creating TODO item', { todo: JSON.parse(event.body) })
     try {
-      const newTodo: CreateTodoRequest = JSON.parse(event.body);
+      const data = JSON.parse(event.body)
+      const valid = ajv.validate(schema, data)
+      if (!valid) {
+        logger.error('VALIDATION', ajv.errors)
+        throw new Error(ajv.errors.toString())
+      }
+      const newTodo: CreateTodoRequest = data
 
-      const userId = getUserId(event);
-    
-      const todo = await createTodo(newTodo, userId);
-  
+      const userId = getUserId(event)
+
+      const todo = await createTodo(newTodo, userId)
+
       return {
         statusCode: 201,
         body: JSON.stringify({
-          item: todo,
+          item: todo
         })
       }
     } catch (error) {
       logger.error('Error: ', error)
-      throw new Error(error);
+      throw new Error(error)
     }
-  })
+  }
+)
 
 handler.use(
   cors({
     credentials: true
   })
 )
+
+const schema = {
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string'
+    },
+    dueDate: {
+      type: 'string'
+    }
+  },
+  required: ['name', 'dueDate'],
+  additionalProperties: false
+}
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -9,32 +9,52 @@ import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { getUserId } from '../utils'
 
 import { createLogger } from '../../utils/logger'
+import Ajv from 'ajv'
 
-
+const ajv = new Ajv()
 const logger = createLogger('createTodo')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     logger.info('Updatting TODO item', { todo: event.body })
     try {
-      const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+      const data = JSON.parse(event.body)
+      const valid = ajv.validate(schema, data)
+      if (!valid) {
+        logger.error('VALIDATION', ajv.errors)
+        throw new Error(ajv.errors.toString())
+      }
+      const updatedTodo: UpdateTodoRequest = data
       const todoId = event.pathParameters.todoId
       const userId = getUserId(event)
-      await updateTodo(updatedTodo, todoId, userId);
+      await updateTodo(updatedTodo, todoId, userId)
       return {
         statusCode: 201,
         body: JSON.stringify({})
       }
     } catch (error) {
       logger.error('Error: ', error)
-      throw new Error(error);
+      throw new Error(error)
     }
-  })
+  }
+)
 
-handler
-  .use(httpErrorHandler())
-  .use(
-    cors({
-      credentials: true
-    })
-  )
+handler.use(httpErrorHandler()).use(
+  cors({
+    credentials: true
+  })
+)
+
+const schema = {
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string'
+    },
+    dueDate: {
+      type: 'string'
+    }
+  },
+  required: ['name', 'dueDate'],
+  additionalProperties: false
+}
