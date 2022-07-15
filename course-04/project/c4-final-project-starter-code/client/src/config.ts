9a30a2435bb484e8e28ed4d3d12cd15d03ff1bec
James Abraham <horlamidex1@gmail.com>
2022-07-15 14:48:51 +0100

    Fixing validation

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '139huplc0j'
+const apiId = 'qrjm6q0k90'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
